620cb9c884ab07d73adbb0bf5058deb78a566f25
shaifat <skshaifatmurshed@gmail.com>
2018-10-06 20:45:44 +0530

    no commit

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>login</title>
+</head>
+<body>
+    
+
+    
+    <form action="login_cont.php" method="post">
+    <a> username </a>
+    <input type="text" name="uname"><br>
+
+    <a> password </a>
+    <input type="password" name="password"><br>
+
+    <input type="submit" name="submit" value="submit">
+    
+    </form>
+
+</body>
+</html>
